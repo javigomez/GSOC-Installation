fc5eabee154844c61f86f7d8b10a5e17ba16a983
Javier <javier.gomez.corio@gmail.com>
2012-08-13 15:59:39 -0600

    Use the 2.5.7 sql file that was already done for the first part of the project

--- installation/sql/mysql/joomla.sql
+++ installation/sql/mysql/joomla.sql
@@ -1681,21 +1681,3 @@ INSERT INTO `#__viewlevels` (`id`, `title`, `ordering`, `rules`) VALUES
 (3, 'Special', 2, '[6,3,8]');
 
 # -------------------------------------------------------
-# ---GSOC------------------------------------------------
-# Insert the update server for Accredited translations if not exist
-INSERT IGNORE INTO
-    `#__update_sites`
-VALUES
-    (10000,
-    'Accredited Joomla! Translations',
-     'collection',
-     'http://update.joomla.org/language/translationlist.xml',
-     1,
-     0);
-
-# Link english lang and the Accredited translations update site for JUpdater needs
-INSERT IGNORE INTO
-    `#__update_sites_extensions`
-VALUES
-	(10000,
-	600);
